486647ea661c14b5ac91383a91e1e476d4297f3f
gh-user01 <94589638+gh-user01@users.noreply.github.com>
2022-07-11 15:19:09 +0800

    asdasd
     this shit

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,71 +2,73 @@ import React from 'react'
 import CustomBtn from './CustomBtn'
 import logo from '../CPU_logo.svg'
 import logoMobile from '../logoMobile.svg'
-import {Toolbar, Typography} from '@material-ui/core'
-import {makeStyles} from "@material-ui/core/styles";
-import {BrowserRouter, Routes, Switch, Route} from 'react-router-dom';
-import Questionnaire from './Questionnaire'
+import { Toolbar, Typography } from '@material-ui/core'
+import { makeStyles } from "@material-ui/core/styles";
+import { Link } from 'react-router-dom';
+
 
 const styles = makeStyles({
-    bar:{
+    bar: {
         paddingTop: "1.15rem",
         backgroundColor: "#fff",
-        ['@media (max-width:780px)']: { 
-           flexDirection: "column"
-          }
+        ['@media (max-width:780px)']: {
+            flexDirection: "column"
+        }
     },
     logo: {
-        width: "5%", 
-        ['@media (max-width:780px)']: { 
-           display: "none"
-           }
+        width: "5%",
+        ['@media (max-width:780px)']: {
+            display: "none"
+        }
     },
-    logoMobile:{
-        width: "100%", 
-        display: "none", 
-        ['@media (max-width:780px)']: { 
+    logoMobile: {
+        width: "100%",
+        display: "none",
+        ['@media (max-width:780px)']: {
             display: "inline-block"
-            }
+        }
     },
     menuItem: {
-        cursor: "pointer", 
+        cursor: "pointer",
         flexGrow: 1,
         "&:hover": {
-            color:  "#4f25c8"
+            color: "#4f25c8"
         },
-        ['@media (max-width:780px)']: { 
-            paddingBottom: "1rem"    }
+        ['@media (max-width:780px)']: {
+            paddingBottom: "1rem"
+        }
     }
 })
 
 function NavBar() {
     const classes = styles()
     return (
-        <BrowserRouter>
-            <Routes>
-            <Toolbar position="sticky" color="rgba(0, 0, 0, 0.87)" className={classes.bar}>   
-                <img src={logo} className={classes.logo}/> 
-                <img src={logoMobile} className={classes.logoMobile}/>
-                <Route path="/questionnaire" component={Questionnaire} />
-                <Typography variant="h6" className={classes.menuItem}>
+        <Toolbar position="sticky" color="rgba(0, 0, 0, 0.87)" className={classes.bar}>
+            <img src={logo} className={classes.logo} />
+            <img src={logoMobile} className={classes.logoMobile} />
+            <Typography variant="h6" className={classes.menuItem}>
+                <Link to="/questionnaire">
                     Modify Questionnaires
-                </Typography>
-                <Typography variant="h6" className={classes.menuItem}>
+                </Link>
+            </Typography>
+            <Typography variant="h6" className={classes.menuItem}>
+                <Link to="/survey">
                     Survey
-                </Typography>
-                <Typography variant="h6" className={classes.menuItem}>
+                </Link>
+            </Typography>
+            <Typography variant="h6" className={classes.menuItem}>
+                <Link to="/profile">
                     Profile
-                </Typography>
-                {/* <Typography variant="h6" className={classes.menuItem}>
-                    Demos 
-                </Typography>
-                <Typography variant="h6" className={classes.menuItem}>
-                    Contact Us 
-                </Typography> */}
-                {/* <CustomBtn txt="Trial Our Product"/> */}
-            </Toolbar>
-            </Routes>
-        </BrowserRouter>
+                </Link>
+            </Typography>
+            {/* <Typography variant="h6" className={classes.menuItem}>
+                Demos
+            </Typography>
+            <Typography variant="h6" className={classes.menuItem}>
+                Contact Us
+            </Typography>
+            <CustomBtn txt="Trial Our Product" /> */}
+        </Toolbar>
     )
 }
 
